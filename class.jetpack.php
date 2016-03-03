94d69d06df12b8c737261903c45689d1f2300dba
Rocco Tripaldi <tripaldir@gmail.com>
2016-03-03 15:30:38 -0500

    Generate Secrets
    Add user id to Jetpack-generated secrets.

--- class.jetpack.php
+++ class.jetpack.php
@@ -4597,7 +4597,7 @@ p {
 			}
 
 			$secrets = Jetpack::init()->generate_secrets();
-			Jetpack_Options::update_option( 'authorize', $secrets[0] . ':' . $secrets[1] . ':' . $secrets[2] );
+			Jetpack_Options::update_option( 'authorize', $secrets[0] . ':' . $secrets[1] . ':' . $secrets[2] . ':' . $secrets[3] );
 
 			@list( $secret ) = explode( ':', Jetpack_Options::get_option( 'authorize' ) );
 
@@ -4970,9 +4970,10 @@ p {
 	 */
 	public function generate_secrets() {
 	    $secrets = array(
-		wp_generate_password( 32, false ), // secret_1
-		wp_generate_password( 32, false ), // secret_2
-		( time() + 600 ), // eol ( End of Life )
+			wp_generate_password( 32, false ), // secret_1
+			wp_generate_password( 32, false ), // secret_2
+			( time() + 600 ), // eol ( End of Life )
+			get_current_user_id(), // ties the secrets to the current user
 	    );
 
 	    return $secrets;
@@ -5043,7 +5044,7 @@ p {
 		add_action( 'pre_update_jetpack_option_register', array( 'Jetpack_Options', 'delete_option' ) );
 		$secrets = Jetpack::init()->generate_secrets();
 
-		Jetpack_Options::update_option( 'register', $secrets[0] . ':' . $secrets[1] . ':' . $secrets[2] );
+		Jetpack_Options::update_option( 'register', $secrets[0] . ':' . $secrets[1] . ':' . $secrets[2] . ':' . $secrets[3] );
 
 		@list( $secret_1, $secret_2, $secret_eol ) = explode( ':', Jetpack_Options::get_option( 'register' ) );
 		if ( empty( $secret_1 ) || empty( $secret_2 ) || empty( $secret_eol ) || $secret_eol < time() ) {
@@ -5073,6 +5074,7 @@ p {
 				'site_lang'       => get_locale(),
 				'timeout'         => $timeout,
 				'stats_id'        => $stats_id,
+				'state'           => get_current_user_id(),
 			),
 			'headers' => array(
 				'Accept' => 'application/json',
